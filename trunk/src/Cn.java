1959b05f74a289c3812c1015a91df2c4bc79b84f
merchante.mariano <merchante.mariano@f7efa0aa-eef7-8fdb-3b7f-bc555c6f7d26>
2010-05-08 03:12:13 +0000

    Algunas mejoras, y agregue Cn. Tambien esta el grafo que nos generaba problemas, es grafo1.graph ...

---
+++ trunk/src/Cn.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+/**
+ * A ciclyc graph
+ * @author Mariano
+ *
+ * @param <T>
+ */
+public class Cn<T> extends Graph<T> {
+
+	public Cn(List<T> values) {
+		super();
+
+		this.addVertex(values.get(0));
+		for (int i = 1; i < values.size(); i++) {
+			this.addVertex(values.get(i));
+			this.addEdge(values.get(i), values.get(i - 1));
+		}
+		this.addEdge(values.get(0), values.get(values.size() - 1));
+
+	}
+}
--- trunk/src/Kn.java
+++ trunk/src/Kn.java
@@ -1,18 +1,24 @@
 import java.util.List;
 
+/**
+ * A complete graph.
+ * @author Mariano
+ *
+ * @param <V>
+ */
 public class Kn<V> extends Graph<V> {
 
-	public Kn(int vertices, List<V> values) {
+	public Kn(List<V> values) {
 		super();
 
 		// Add vertices
-		for (int i = 0; i < vertices; i++) {
+		for (int i = 0; i < values.size(); i++) {
 			this.addVertex(values.get(i));
 		}
 
 		// Add edges
-		for (int i = 0; i < vertices; i++) {
-			for (int j = 0; j < vertices; j++)
+		for (int i = 0; i < values.size(); i++) {
+			for (int j = 0; j < values.size(); j++)
 				if (j != i)
 					this.addEdge(values.get(i), values.get(j));
 		}
